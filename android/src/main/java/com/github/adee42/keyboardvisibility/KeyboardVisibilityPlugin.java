71208c765d613f67765bb7698006231048c67051
Maksym Gerasymchuk <maxgerasimchuk@gmail.com>
2019-09-09 22:17:58 +0300

    Don't register the plugin for background FlutterViews

--- android/src/main/java/com/github/adee42/keyboardvisibility/KeyboardVisibilityPlugin.java
+++ android/src/main/java/com/github/adee42/keyboardvisibility/KeyboardVisibilityPlugin.java
@@ -99,7 +99,11 @@ public class KeyboardVisibilityPlugin implements StreamHandler, Application.Acti
     }
 
     public static void registerWith(Registrar registrar) {
-
+        if (registrar.activity() == null) {
+            // When a background flutter view tries to register the plugin, the registrar has no activity.
+            // We stop the registration process as this plugin is foreground only.
+            return;
+        }
         final EventChannel eventChannel = new EventChannel(registrar.messenger(), STREAM_CHANNEL_NAME);
         KeyboardVisibilityPlugin instance = new KeyboardVisibilityPlugin(registrar);
         eventChannel.setStreamHandler(instance);
